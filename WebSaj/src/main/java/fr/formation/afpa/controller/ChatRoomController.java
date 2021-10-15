5ff95e2d5f161ed9db548bb20a51aecaa98f7300
SoundsZero <78345779+SoundsZero@users.noreply.github.com>
2021-10-15 12:34:43 +0200

    [saj][init][ag] - init projet saj

--- WebSaj/src/main/java/fr/formation/afpa/controller/ChatRoomController.java
+++ WebSaj/src/main/java/fr/formation/afpa/controller/ChatRoomController.java
@@ -17,27 +17,27 @@ import fr.formation.afpa.domain.ChatMessage;
 @Controller
 public class ChatRoomController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ChatRoomController.class);
-    @Autowired
-    private SimpMessageSendingOperations messagingTemplate;
+	private static final Logger logger = LoggerFactory.getLogger(ChatRoomController.class);
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
 
-    @MessageMapping("/chat/{roomId}/sendMessage")
-    public void sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
-        logger.info(roomId+" Chat messahe recieved is "+chatMessage.getContent());
-        messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
-    }
+	@MessageMapping("/chat/{roomId}/sendMessage")
+	public void sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
+		logger.info(roomId + " Chat message recieved is " + chatMessage.getContent());
+		messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
+	}
 
-    @MessageMapping("/chat/{roomId}/addUser")
-    public void addUser(@DestinationVariable String roomId, @Payload ChatMessage chatMessage,
-                        SimpMessageHeaderAccessor headerAccessor) {
-        String currentRoomId = (String) headerAccessor.getSessionAttributes().put("room_id", roomId);
-        if (currentRoomId != null) {
-        	ChatMessage leaveMessage = new ChatMessage();
-            leaveMessage.setType(ChatMessage.MessageType.LEAVE);
-            leaveMessage.setSender(chatMessage.getSender());
-            messagingTemplate.convertAndSend(format("/chat-room/%s", currentRoomId), leaveMessage);
-        }
-        headerAccessor.getSessionAttributes().put("name", chatMessage.getSender());
-        messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
-    }
+	@MessageMapping("/chat/{roomId}/addUser")
+	public void addUser(@DestinationVariable String roomId, @Payload ChatMessage chatMessage,
+			SimpMessageHeaderAccessor headerAccessor) {
+		String currentRoomId = (String) headerAccessor.getSessionAttributes().put("room_id", roomId);
+		if (currentRoomId != null) {
+			ChatMessage leaveMessage = new ChatMessage();
+			leaveMessage.setType(ChatMessage.MessageType.LEAVE);
+			leaveMessage.setSender(chatMessage.getSender());
+			messagingTemplate.convertAndSend(format("/chat-room/%s", currentRoomId), leaveMessage);
+		}
+		headerAccessor.getSessionAttributes().put("name", chatMessage.getSender());
+		messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
+	}
 }
